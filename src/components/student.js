dfb0aca056af4a43fdb43fe24843d20c16cfc2e9
mundoe1999 <estebanmundo1999@gmail.com>
2019-06-03 14:19:03 -0400

    Started react project

---
+++ src/components/student.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Student extends Component{
+  render(){
+    return (
+      <div>
+        test
+      </div>
+    );
+  }
+}
+
+export default Student;
+

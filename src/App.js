d76c12a9bc441582a718b0638a5cfdb175a321c3
mundoe1999 <estebanmundo1999@gmail.com>
2019-06-04 14:03:46 -0400

    Updated Landing page to reflect Mock-up

--- src/App.js
+++ src/App.js
@@ -6,24 +6,31 @@ import Student from './components/student';
 import logo from './logo.svg';
 import './App.css';
 
+
+class NavBar extends Component{
+  render(){
+    return(
+      <nav>
+        <div className="flex-container">
+          <div className="logo">CAMPUS<strong>CONNECT</strong></div>
+          <ul>
+
+            <li>Students</li>
+            <li>Schools</li>
+          </ul>
+        </div>
+
+      </nav>
+    );
+  }
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Student />
-      </header>
+      <NavBar />
+      <div className="App-header"></div>
+
     </div>
   );
 }
